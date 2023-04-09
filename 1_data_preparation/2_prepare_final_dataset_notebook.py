fa8d8b4f512263e7c73555abff5aedb669097135
brutovsky <vadim.nakitnyak@gmail.com>
2023-04-09 16:10:03 +0300

    add create_dummy function for categorical data test gradient_boosting with feature estimation

--- 1_data_preparation/2_prepare_final_dataset_notebook.py
+++ 1_data_preparation/2_prepare_final_dataset_notebook.py
@@ -75,7 +75,6 @@ df_isw.drop(columns=["Date", "Name"], inplace=True)
 df_isw.head(1)
 
 # %% cell_id="3b3a59a75bb2437c86abb073be054278" deepnote_cell_type="code"
-
 df_isw["date_tomorrow_datetime"] = df_isw["report_date"].apply(
     lambda x: x + datetime.timedelta(days=1)
 )
@@ -442,22 +441,43 @@ df_weather_v5.head(1)
 # ### Final merge preparations
 
 # %%
-df_weather_v6 = pd.get_dummies(
-    df_weather_v5, columns=["day_of_week"], prefix=["day_of_week"]
-)
+df_weather_v5.shape
 
-df_weather_v6[
-    [
-        "day_datetime",
-        "day_of_week_Monday",
-        "day_of_week_Tuesday",
-        "day_of_week_Wednesday",
-        "day_of_week_Thursday",
-        "day_of_week_Friday",
-        "day_of_week_Saturday",
-        "day_of_week_Sunday",
-    ]
-].head(10)
+# %%
+from typing import Tuple
+
+# Defining the function for dummy creation
+def create_dummy(df: pd.DataFrame, dummy_var_list: list) -> Tuple:
+    """
+    Creates dummy variables for the variables in dummy_var_list
+    Returns a tuple of the following
+        * df - The dataframe with the dummy variables
+        * dummy_var_list - The list of dummy variables created
+    """
+    # Placeholder for the dummy variables
+    added_features = []
+    for var in dummy_var_list:
+        dummy = pd.get_dummies(df[var], prefix=var, drop_first=False)
+
+        # Adding the new features to list
+        added_features.extend(dummy.columns)
+
+        # Adding the dummy variables to the dataframe
+        df = pd.concat([df, dummy], axis=1)
+        df.drop(var, axis=1, inplace=True)
+
+    # Returning the dataframe
+    return df, added_features
+
+
+# %%
+df_weather_v6, categorical_features = create_dummy(df_weather_v5.copy(), ["day_of_week", "hour_conditions"])
+print(categorical_features)
+print(len(categorical_features))
+
+# %%
+print(df_weather_v6.shape)
+df_weather_v6.head(5)
 
 # %%
 df_weather_v6.drop(["day_datetime"], axis=1, inplace=True)
@@ -470,19 +490,13 @@ df_weather_v6.head(1)
 # df_weather_v6.fillna(0, inplace=True)
 
 # %%
-# Handle categorical data
-df_weather_v6["hour_conditions"] = pd.Categorical(df_weather_v6["hour_conditions"])
-df_weather_v6["hour_conditions_code"] = df_weather_v6["hour_conditions"].cat.codes
-df_weather_v6[["hour_conditions", "hour_conditions_code"]].head(5)
-
-# %%
 # drop all columns that are of type
 df_weather_v6 = df_weather_v6.select_dtypes(exclude=['object'])
 
 # %%
-# drop column "hour_conditions"
+# drop columns
 df_weather_v6.drop(
-    ["hour_conditions", "event_intersection_alarm_id"], axis=1, inplace=True
+    ["event_intersection_alarm_id"], axis=1, inplace=True
 )
 # TODO: revert back dropped columns
 df_weather_v6.drop(
