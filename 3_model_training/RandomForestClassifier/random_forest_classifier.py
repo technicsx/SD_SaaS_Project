a3fe871ab4b72bf638f20c148647ac0e373bbcfb
brutovsky <vadim.nakitnyak@gmail.com>
2023-04-09 16:10:22 +0300

    update random_forrest_classifier with feature estimation

--- 3_model_training/RandomForestClassifier/random_forest_classifier.py
+++ 3_model_training/RandomForestClassifier/random_forest_classifier.py
@@ -1,50 +1,33 @@
+# %%
 import numpy as np
 import pandas as pd
 import os
 
 from paths_full import DATA_PREP_RESULTS_FOLDER
 
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
+from matplotlib import pyplot
 
-csv_path = os.path.join(DATA_PREP_RESULTS_FOLDER, "df_weather_v7.csv")
-df_final = pd.read_csv(csv_path)
 
-# Fill NaN values
-df_final[["event_all_region"]] = df_final[["event_all_region"]].fillna(value=0)
+pickle_path = os.path.join(DATA_PREP_RESULTS_FOLDER, "df_weather_v7.pkl")
+df_final = pd.read_pickle(pickle_path)
 
-X = np.array(
-    df_final[
-        [
-            "region_id",
-            "event_all_region",
-            "day_datetimeEpoch",
-            "hour_datetimeEpoch",
-            "ukrainian_holiday",
-            "russian_holiday",
-            "hour_temp",
-            "hour_snow",
-            "hour_visibility",
-            "hour_conditions_code",
-            "lunar_eclipse",
-            "solar_eclipse",
-            "moonphased_eclipse",
-            "alarmed_regions_count",
-        ]
-    ]
-)
-y = np.array(df_final["is_alarm"])
+# %%
+y = df_final["is_alarm"].values
+df_final.drop(["is_alarm"], axis=1, inplace=True)
+X = np.array(df_final)
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.25, random_state=101
+    X, y, test_size=0.2, random_state=101
 )
 
+# %%
 clf = RandomForestClassifier(n_estimators=10)
 clf = clf.fit(X_train, y_train)
 
 print(clf.score(X_test, y_test))
-
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 # Predict the labels for the test set
 y_pred = clf.predict(X_test)
@@ -56,3 +39,21 @@ disp = ConfusionMatrixDisplay(
     confusion_matrix=confusion_matrix, display_labels=clf.classes_
 )
 disp.plot()
+
+# %%
+# get importance
+importance = clf.feature_importances_
+# summarize feature importance
+for i,v in enumerate(importance):
+ print('Feature: %0d, Score: %.5f' % (i,v))
+# plot feature importance
+pyplot.bar([x for x in range(len(importance))], importance)
+pyplot.show()
+
+# %%
+feat_importances = pd.Series(clf.feature_importances_, index=df_final.columns)
+feat_importances.nlargest(20).plot(kind='barh')
+pyplot.title("Top 20 important features")
+pyplot.show()
+
+# %%
